28b3faa0a7538151b104c84de7ec897c05945c11
simon_office_laptop <who023me@hotmail.com>
2018-06-25 10:42:12 +0800

    android input ui study

---
+++ 02/2.3/InputUI/app/src/main/java/com/simon/jia/inputui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.simon.jia.inputui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
